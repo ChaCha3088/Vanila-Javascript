2ddc43684735ddcdafe9eddd0005a22dfe2053e1
Cha Cha <cha3088@gmail.com>
2022-03-19 18:12:19 +0900

    main

--- js/weather.js
+++ js/weather.js
@@ -2,12 +2,15 @@ const API_KEY = 'd7b61c090e813c70c90d8cc01c81e400';
 
 function onGeoOk(position) {
     const lat = position.coords.latitude;
+    console.log(lat);
     const long = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat={${lat}}&lon={${long}}&appid={${API_KEY}}&units=metric`;
+    console.log(long);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat={${lat}}&lon={${long}}&appid={c6ff18221a167b9ba981bb72a6797912}&units=metric`;
     const result = fetch(url);
     result.then(response => response.json()).then(data => {
-        const weather = document.querySelector('#weather span:first-child')
-        const city = document.querySelector('#weather span:last-child')
+        console.log(data);
+        const weather = document.querySelector('#weather span:first-child');
+        const city = document.querySelector('#weather span:last-child');
         weather.innerText = `${data.weather[0].main1} / ${data.main.temp}°C`;
         city.innerText = data.name;
     });
